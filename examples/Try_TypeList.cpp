bec82725a5c17152bf5ecd1d65899c3680ded6f7
Marco Schuler <marco.schuler@sensirion.com>
2018-01-15 09:41:02 +0000

    Implement length of TypeList

--- examples/Try_TypeList.cpp
+++ examples/Try_TypeList.cpp
@@ -1,17 +1,14 @@
+#include <iostream>
 #include "etl-98/TypeList.h"
 
-typedef TypeList<double, TypeList<float, NullType> > FloatingTypes;
-typedef TypeList<char, TypeList<int, NullType> > IntTypes;
-
-template<typename T>
-class OutputClass
-{
-  void Output(T val)
-  {
-  }
-};
+typedef etl98::TypeList<double, etl98::TypeList<float, etl98::NullType> > FloatingTypes;
+typedef etl98::TypeList<char, etl98::TypeList<short, etl98::TypeList<int, etl98::NullType> > > IntegerTypes;
 
 int main()
 {
+  std::cout << etl98::Length<FloatingTypes>::value << std::endl;
+  std::cout << etl98::Length<IntegerTypes>::value << std::endl;
+
+  return 0;
 }
 
--- include/etl-98/TypeList.h
+++ include/etl-98/TypeList.h
@@ -2,14 +2,61 @@
 #define ETL_98_TYPE_LIST_H__
 
 namespace etl98 {
+
+// A type representing Null. Can be used to mark certain border cases.
+// Usually there is no objects created of type NullType. It basically indicates:
+// "I am not an interesting type."
 class NullType {};
 
+///////////////////////////////////////////////////////////////////////////////
+// Class template TypeList
+//
+// Building block for typelist of any length.
+//
+// Instantiation example:
+// typedef TypeList<float, TypeList<double, NullType> > FloatTypes;
+//
+// Defines nested types:
+// * Head (first element, a non-typelist type by convention)
+// * Tail (second element, can be another typelist or NullType)
+///////////////////////////////////////////////////////////////////////////////
 template<typename H, typename T>
 struct TypeList
 {
   H head;
   T tail;
 };
+
+///////////////////////////////////////////////////////////////////////////////
+// Class template Length
+//
+// Computes the length of a TypeList.
+//
+// Invocation (TList is a typelist):
+// Length<TList>::value
+//
+// Returns a compile-time constant containing the length of TList. The
+// end-terminator is not counted (by convention of type NullType).
+///////////////////////////////////////////////////////////////////////////////
+
+// Definition of template
+template<typename TList>
+struct Length;
+
+// Specialized implementation for tail == NullType
+template<>
+struct Length<NullType>
+{
+  static const int value = 0;
+};
+
+// Generic implementation
+template<typename T, typename U>
+struct Length<TypeList<T, U> >
+{
+  static const int value = 1 + Length<U>::value;
+};
+
 }
 
 #endif
