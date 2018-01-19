7bb19bd6675e0d01186402a8678e5f1c49b04209
Marco Schuler <marco.schuler@sensirion.com>
2018-01-19 15:10:30 +0000

    Add tests for Typelist

---
+++ test/tests/Test_Typelist.cpp
@@ -0,0 +1,46 @@
+#include "etl-98/Typelist.h"
+
+#include "CppUTest/TestHarness.h"
+
+TEST_GROUP(Typelist)
+{
+  void setup()
+  {
+  }
+
+  void teardown()
+  {
+  }
+};
+
+typedef etl98::Typelist<int, etl98::NullType> ListOfIntOfLength1;
+typedef etl98::Typelist<int, etl98::Typelist<int, etl98::NullType> > ListOfIntOfLength2;
+typedef etl98::Typelist<int, etl98::Typelist<int, etl98::Typelist<int, etl98::NullType> > > ListOfIntOfLength3;
+
+typedef etl98::Typelist<char, etl98::Typelist<short, etl98::Typelist<int, etl98::NullType> > > ListOfIntegers;
+TEST(Typelist, LengthIsCorrect)
+{
+  int length = etl98::Length<ListOfIntOfLength1>::value;
+  CHECK_EQUAL(length, 1);
+
+  length = etl98::Length<ListOfIntOfLength2>::value;
+  CHECK_EQUAL(length, 2);
+  length = etl98::Length<ListOfIntOfLength3>::value;
+  CHECK_EQUAL(length, 3);
+}
+
+TEST(Typelist, ContainsIsCorrectForAllTypesInList)
+{
+  bool contains = etl98::Contains<ListOfIntegers, char>::value;
+  CHECK_EQUAL(contains, true);
+  contains = etl98::Contains<ListOfIntegers, short>::value;
+  CHECK_EQUAL(contains, true);
+  contains = etl98::Contains<ListOfIntegers, int>::value;
+  CHECK_EQUAL(contains, true);
+}
+
+TEST(Typelist, ContainsIsCorrectForTypeNotInLis)
+{
+  bool contains = etl98::Contains<ListOfIntegers, float>::value;
+  CHECK_EQUAL(contains, false);
+}
