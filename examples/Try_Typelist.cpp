d7f94b0f70513fb3e0f5f67ff72bfc5c44febc48
0xB767B <marco.schuler@gmail.com>
2018-01-23 15:29:00 +0100

    Add operations to namespace

--- examples/Try_Typelist.cpp
+++ examples/Try_Typelist.cpp
@@ -6,14 +6,14 @@ typedef etl98::Typelist<char, etl98::Typelist<short, etl98::Typelist<int, etl98:
 
 int main()
 {
-  std::cout << etl98::Length<FloatingTypes>::value << std::endl;
-  std::cout << etl98::Length<IntegerTypes>::value << std::endl;
+  std::cout << etl98::TypelistOps::Length<FloatingTypes>::value << std::endl;
+  std::cout << etl98::TypelistOps::Length<IntegerTypes>::value << std::endl;
 
-  std::cout << etl98::Contains<FloatingTypes, float>::value << std::endl;
-  std::cout << etl98::Contains<FloatingTypes, int>::value << std::endl;
+  std::cout << etl98::TypelistOps::Contains<FloatingTypes, float>::value << std::endl;
+  std::cout << etl98::TypelistOps::Contains<FloatingTypes, int>::value << std::endl;
 
-  std::cout << etl98::Contains<IntegerTypes, float>::value << std::endl;
-  std::cout << etl98::Contains<IntegerTypes, int>::value << std::endl;
+  std::cout << etl98::TypelistOps::Contains<IntegerTypes, float>::value << std::endl;
+  std::cout << etl98::TypelistOps::Contains<IntegerTypes, int>::value << std::endl;
 
   return 0;
 }
