886209c3e34b92b61e9cb77599ab5a4c50f67278
0xB767B <marco.schuler@gmail.com>
2018-01-25 18:09:52 +0100

    Use absolute path again in youcompleteme provider

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -1,6 +1,6 @@
 def FlagsForFile( filename, **kwargs ):
   return {
     'flags': [ '-x', 'c++', '-Wall', '-Wextra', '-Werror', '-std=c++98',
-    '-I./include/', '-I./libs/cpputest/include/'],
+    '-I/home/borealis/work/etl-98/include/', '-I/home/borealis/work/etl-98/libs/cpputest/include/'],
   }
 
