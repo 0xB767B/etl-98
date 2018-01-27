a8dcd5d2c28771c1d648a7693efb9326d80e69c7
0xB767B <marco.schuler@gmail.com>
2018-01-27 12:15:40 +0100

    Change youcompleteme provider
    Replace .ycm_extra_conf.py with compilation_commands.json generated with bear.

--- .ycm_extra_conf.py
+++
@@ -1,6 +0,0 @@
-def FlagsForFile( filename, **kwargs ):
-  return {
-    'flags': [ '-x', 'c++', '-Wall', '-Wextra', '-Werror', '-std=c++98',
-    '-I/home/borealis/work/etl-98/include/', '-I/home/borealis/work/etl-98/libs/cpputest/include/'],
-  }
-
