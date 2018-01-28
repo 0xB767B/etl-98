15082c5e978d71668061599f819c97c3b9355ce5
0xB767B <marco.schuler@gmail.com>
2018-01-28 03:54:46 +0100

    Add support for codecov.io

--- test/clean-coverage.sh
+++ test/clean-coverage.sh
@@ -2,5 +2,5 @@
 
 # Todo: This should obviously be handled by scons
 
-rm -rf *.gcda *.gcno coverage-report coverage.info tests/*.gcda tests/*.gcno
+rm -rf *.gcov *.gcda *.gcno coverage-report coverage.info tests/*.gcda tests/*.gcno
 
