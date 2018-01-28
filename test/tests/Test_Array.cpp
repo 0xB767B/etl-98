0dab278ecc8b8ec10847fbab0ebf61cc065ff37e
0xB767B <marco.schuler@gmail.com>
2018-01-28 01:55:43 +0100

    Make test more explcit

--- test/tests/Test_Array.cpp
+++ test/tests/Test_Array.cpp
@@ -6,9 +6,11 @@ TEST_CASE("Array", "[array]")
   SECTION("Array initialization works")
   {
     Array<int, 5> a = { 0, 1, 2, 3, 4 };
-    for(int i = 0; i < 5; i++) {
-      REQUIRE(i == a[i]);
-    }
+    REQUIRE(0 == a[0]);
+    REQUIRE(1 == a[1]);
+    REQUIRE(2 == a[2]);
+    REQUIRE(3 == a[3]);
+    REQUIRE(4 == a[4]);
   }
 }
 
