0fae79a097f45e3f151243581898cdba4451c6a2
0xB767B <marco.schuler@gmail.com>
2018-01-28 11:14:51 +0100

    Ignore catch.hpp

--- test/generate-coverage-report.sh
+++ test/generate-coverage-report.sh
@@ -5,6 +5,7 @@
 rm -rf *.gcda *.gcno coverage-report coverage.info tests/*.gcda tests/*.gcno
 scons -c
 scons check
-lcov --capture --no-external --directory . --directory ../include --output-file coverage.info
+lcov --capture --no-external --remove "catch\.hpp" --directory . --directory ../include --output-file coverage.info
+
 genhtml coverage.info --css-file solarized-dark.css --output-directory coverage-report
 
